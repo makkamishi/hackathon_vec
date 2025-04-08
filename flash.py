0effec0bb6f37552ed2f31790842ddb4a4c4cd94
makkamishi <naveenben77@gmail.com>
2025-04-08 20:47:42 +0530

    Add files via upload

---
+++ flash.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import mysql.connector
+from mysql.connector import Error
+
+app = Flask(__name__)
+
+# Database connection parameters
+db_config = {
+    'user':'avnadmin',
+    'password':'AVNS_cjiPLX8eX4XKlgCiKnU',
+    'host':'mysql-25dad718-jhonjeri92-ebde.k.aivencloud.com',
+    'port':'19558',
+    'database':'defaultdb'
+}
+
+@app.route('/insert_data', methods=['POST'])
+def insert_data():
+    distance = request.form.get('distance')
+    if distance is None:
+        return "Distance value is missing", 400
+
+    try:
+        # Establish a database connection
+        conn = mysql.connector.connect(**db_config)
+        if conn.is_connected():
+            cursor = conn.cursor()
+            # Insert the distance value into the sensor_data table
+            cursor.execute("INSERT INTO sensor_data (distance) VALUES (%s)", (distance,))
+            conn.commit()
+            return "New record created successfully", 201
+    except Error as e:
+        return f"Error: {e}", 500
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
