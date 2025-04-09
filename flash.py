0ab9f2afb278d0e19becdec58681b356b7f28f10
makkamishi <naveenben77@gmail.com>
2025-04-09 09:28:44 +0530

    flash.py

--- flash.py
+++ flash.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
 import mysql.connector
 from mysql.connector import Error
+import json
 
 app = Flask(__name__)
 
-# Database connection parameters
 db_config = {
     'user':'avnadmin',
     'password':'AVNS_cjiPLX8eX4XKlgCiKnU',
@@ -16,24 +16,49 @@ db_config = {
 @app.route('/insert_data', methods=['POST'])
 def insert_data():
     distance = request.form.get('distance')
+    id = request.form.get('id')  
+
     if distance is None:
         return "Distance value is missing", 400
 
     try:
-        # Establish a database connection
         conn = mysql.connector.connect(**db_config)
         if conn.is_connected():
             cursor = conn.cursor()
-            # Insert the distance value into the sensor_data table
-            cursor.execute("INSERT INTO Sensor_data (Percentage) VALUES (%s)", (distance,))
+            cursor.execute("Update Sensor_datas SET Percentage =%s Where id = %s", (distance,id,))
             conn.commit()
             return "New record created successfully", 201
     except Error as e:
         return f"Error: {e}", 500
+        
     finally:
         if conn.is_connected():
             cursor.close()
             conn.close()
 
+def get_sensor_data_as_json():
+
+    conn = mysql.connector.connect(
+    user='avnadmin',
+    password='AVNS_cjiPLX8eX4XKlgCiKnU',
+    host='mysql-25dad718-jhonjeri92-ebde.k.aivencloud.com',
+    port=19558,
+    database='defaultdb'
+    )
+
+    cursor = conn.cursor()
+
+    query = "SELECT Percentage FROM Sensor_datas"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    data = {}
+    for i, row in enumerate(results, start=1):
+        key = f"bin{i}"
+        data[key] = row[0]
+
+    json_data = json.dumps(data, indent=4)
+    return json_data
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    print(get_sensor_data_as_json())
